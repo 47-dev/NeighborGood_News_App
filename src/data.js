38587846b6b4542af4ddc502ea9ff1479365d6d6
RAJVEER SINGH <syrajveer639@gmail.com>
2024-06-18 16:10:53 +0530

    Pagination correction

--- src/data.js
+++ src/data.js
@@ -2,9 +2,9 @@
 export const apiUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5ee6c537618c42ea9eda8aad654f0641";
 
 export const filterData = [
-    { id: "1", title: "Health" },
+    { id: "1", title: "Technology" },
     { id: "2", title: "Business" },
-    { id: "3", title: "Technology" },
+    { id: "3", title: "Health" },
     { id: "4", title: "Entertainment" },
     { id: "5", title: "Sports" },
     { id: "6", title: "Science" }
